f98f2f6023f9b0ef71520a5bff949dd9a9ee0203
chandanch <chandan.cec107@gmail.com>
2019-12-29 19:09:18 +0530

    specs: template binding

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { HeroComponent } from './hero.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+describe('HeroComponent (shallow test', () => {
+    let fixture: ComponentFixture<HeroComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        });
+
+        fixture = TestBed.createComponent(HeroComponent);
+    });
+
+    it('should have right hero', () => {
+        fixture.componentInstance.hero = {id: 1, name: 'SuperMan', strength: 3};
+
+        expect(fixture.componentInstance.hero.name).toEqual('SuperMan');
+    });
+
+    it('should bind name in anchor tag', () => {
+        fixture.componentInstance.hero = {id: 1, name: 'SuperMan', strength: 3};
+        fixture.detectChanges();
+
+        expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperMan');
+    })
+});
