b677476d293964b8d150a6c4e9ca563ed81cf8d3
chandanch <chandan.cec107@gmail.com>
2020-01-01 12:45:01 +0530

    refactor: use of directive stub

--- src/app/heroes/heros.component.deep.spec.ts
+++ src/app/heroes/heros.component.deep.spec.ts
@@ -1,11 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from '../hero.service';
-import { NO_ERRORS_SCHEMA, Component, Input, Output } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Component, Input, Output, Directive, HostListener } from '@angular/core';
 import { of } from 'rxjs/index';
 import { Hero } from '../hero';
 import { By } from '@angular/platform-browser';
 import { HeroComponent } from '../hero/hero.component';
+
+@Directive({
+    selector: '[routerLink]',
+    // host: { '(click)': 'onClick()' }
+})
+export class RouterLinkDirectiveStub {
+    @Input('routerLink') linkParams: any;
+    navigatedTo: any = null;
+
+    @HostListener('click') onClick() {
+        this.navigatedTo = this.linkParams;
+    }
+}
 
 describe('HerosComponent (deep test)', () => {
     let fixture: ComponentFixture<HeroesComponent>;
@@ -22,12 +35,13 @@ describe('HerosComponent (deep test)', () => {
         TestBed.configureTestingModule({
             declarations: [
                 HeroesComponent,
-                HeroComponent
+                HeroComponent,
+                RouterLinkDirectiveStub
             ],
             providers: [
                 { provide: HeroService, useValue: mockHeroService }
             ],
-            schemas: [NO_ERRORS_SCHEMA]
+            // schemas: [NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(HeroesComponent);
     });
